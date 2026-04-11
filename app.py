a6b41f85ca049e4b72a42c521bf7dc4b26f76f75
MelyzzHF <mf7654874@gmail.com>
2026-04-11 00:13:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hola Mundo Avance Proyecto Final --  App en Docker</h1><p>Servidor corriendo exitosamente con Flask y Nginx.</p>"
+    return "<h1>Avance Proyecto Final --  App en Docker</h1><p>Servidor corriendo exitosamente con Flask y Nginx.</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)

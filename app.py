4f5c3d4e54e76fcb378aa7aa1be4eb6914bcf57d
Ubuntu <ubuntu@ip-172-31-24-120.ec2.internal>
2026-04-11 05:18:10 +0000

    feat: despliegue exitoso de arquitectura docker multi-contenedor con Nginx y Flask

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import boto3
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>Avance Proyecto Final --  App en Docker</h1><p>Servidor corriendo exitosamente con Flask y Nginx.</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
